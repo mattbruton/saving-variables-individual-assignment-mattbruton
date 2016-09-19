eef1e37e43fa863952350b3dce344adb14fbd8de
MATT BRUTON <the.bruton@gmail.com>
2016-09-18 19:30:51 -0500

    fixed issue with SavingVariables.Tests that would prevent Moq or Entity from working

--- SavingVariables.Tests/DAL/VariableRepositoryTests.cs
+++ SavingVariables.Tests/DAL/VariableRepositoryTests.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SavingVariables.Models;
+using Moq;
+using System.Data.Entity;
 
 namespace SavingVariables.Tests.DAL
 {
     [TestClass]
     public class VariableRepositoryTests
     {
+
         [TestMethod]
         public void TestMethod1()
         {
--- SavingVariables/DAL/VariableRepository.cs
+++ SavingVariables/DAL/VariableRepository.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SavingVariables.Models;
-using System.Data.Entity;
 
 namespace SavingVariables.DAL
 {
     public class VariableRepository
     {
+        public VariableContext Context { get; set; }
+        public VariableRepository()
+        {
+            Context = new VariableContext();
+        }
+        public VariableRepository(VariableContext _context)
+        {
+            Context = _context;
+        }
     }
 }
