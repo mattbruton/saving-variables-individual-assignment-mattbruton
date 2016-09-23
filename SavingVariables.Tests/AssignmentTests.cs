7dbfa84c9d99c6aa110c76f1d67fdbc39172ea4d
MATT BRUTON <the.bruton@gmail.com>
2016-09-23 17:07:52 -0500

    added 2 more tests for assignment class

--- SavingVariables.Tests/AssignmentTests.cs
+++ SavingVariables.Tests/AssignmentTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
 
 namespace SavingVariables.Tests
 {
@@ -15,9 +16,27 @@ namespace SavingVariables.Tests
         }
 
         [TestMethod]
-        public void AssignmentEnsure()
+        public void AssignmentEnsureAssignmentPropertiesChangedCorrectlyForMethodThatModifiesBoth()
         {
+            Assignment test = new Assignment();
+            test.CheckIfInputMatchesAssignmentPattern("a=2");
 
+            string expected_variable_name = "a";
+            int expected_variable_value = 2;
+            string actual_variable_name = test.AssignmentVariable;
+            int actual_variable_value = test.AssignmentValue;
+
+            Assert.AreEqual(expected_variable_name, actual_variable_name);
+            Assert.AreEqual(expected_variable_value, actual_variable_value);
+        }
+
+        [TestMethod]
+        public void AssignmentEnsureMatchAssignmentWillFailIfInputDoesNotMatch()
+        {
+            Assignment test = new Assignment();
+            test.CheckIfInputMatchesAssignmentPattern("aa=2");
+
+            Assert.IsFalse(test.IsInputValid);
         }
     }
 }
